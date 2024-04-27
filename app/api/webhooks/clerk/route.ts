dc5c5c64fa8284a0f032f8944e1f67eeb0ffe347
Subhajit Kundu <kundusubhajit73@gmail.com>
2024-04-27 14:30:06 +0530

    fixed types error in server actions

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -64,12 +64,12 @@ export async function POST(req: Request) {
       evt.data;
 
     const user = {
-      clerkId: id,
-      email: email_addresses[0].email_address,
+      clerkId: id!,
+      email: email_addresses[0].email_address!,
       username: username!,
-      firstName: first_name,
-      lastName: last_name,
-      photo: image_url,
+      firstName: first_name || '',
+      lastName: last_name || '',
+      photo: image_url!,
     };
 
     const newUser = await createUser(user);
@@ -91,10 +91,10 @@ export async function POST(req: Request) {
     const { id, image_url, first_name, last_name, username } = evt.data;
 
     const user = {
-      firstName: first_name,
-      lastName: last_name,
+      firstName: first_name!,
+      lastName: last_name!,
       username: username!,
-      photo: image_url,
+      photo: image_url!,
     };
 
     const updatedUser = await updateUser(id, user);
