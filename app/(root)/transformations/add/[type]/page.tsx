c48c757c29a87e1c5731701a5a60b2c5a11cc496
Subhajit Kundu <kundusubhajit73@gmail.com>
2024-04-28 02:04:51 +0530

    ready to write business logic of the app

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import Header from '@/components/shared/Header';
+import { transformationTypes } from '@/constants';
+import TransformationForm from '@/components/shared/TransformationForm';
+import { auth } from '@clerk/nextjs/server';
+import { getUserById } from '@/lib/actions/user.action';
 
-const page = ({params}:{params:{type: string}}) => {
+const page = async ({ params }: { params: { type: string } }) => {
+  //it is a object de-structuring we have get it by same name
+  const { userId } = auth();
+  const user = await getUserById(userId as string);
+  const type: string = params.type;
+  const transformation = transformationTypes[type];
   return (
-    <div>page - {params.type}</div>
-  )
-}
+    <>
+      <Header title={transformation.title} subtitle={transformation.subTitle} />
+      <TransformationForm
+        action='Add'
+        userId={user._id}
+        type={transformation as TransformationTypeKey}
+        creditBalance={user.creditBalance}
+      />
+    </>
+  );
+};
 
-export default page
+export default page;
